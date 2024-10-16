cc91d5b1c20875560f364c37805a2885da685bde
kshitig <kshitigvseth@gmail.com>
2024-10-16 17:18:17 -0400

    main start_game program

---
+++ start_game.rb
@@ -0,0 +1,16 @@
+# Load all Ruby files in the lib directory
+Dir[File.expand_path('lib/*.rb', File.dirname(__FILE__))].each do |file|
+    require file
+  end
+    
+# require 'pry'
+
+begin
+    game = Game.new
+    game.start
+rescue StandardError => e
+    puts "An error occurred: #{e.message}"
+ensure
+    puts "Thanks for playing! Your final size was #{game.snake.size}\n\n"
+end
+  
